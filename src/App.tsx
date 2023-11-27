f6f81507017b4d7222b9d2fdb26edbfd9462adba
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-27 13:56:47 +0100

    Style header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Header } from './components/Header';
-import { Box } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 export const Header = () => {
     return (
-        <Box bg='#6B46C1' color='#F7FAFC' w='100%'>
-            <Heading>Simple pricing for your business</Heading>
-            <Text>Plans that are carefully crafted to suit your business.</Text>
+        <Box bg='#6B46C1' color='#F7FAFC' pt='5.53em' pb='15.39em'>
+            <Heading textAlign='center' fontSize='3rem' fontWeight='800'>Simple pricing for your business</Heading>
+            <Text textAlign='center' mt='1em' fontSize='1.5rem' fontWeight='500'>Plans that are carefully crafted to suit your business.</Text>
         </Box>
     )
 }
