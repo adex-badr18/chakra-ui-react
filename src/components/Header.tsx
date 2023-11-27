dd768bbe9fc30b8ca75cce77b64d324e5015ea0c
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-27 13:48:33 +0100

    Create header section markup

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export const Header = () => {
+    return (
+        <Box bg='#6B46C1' color='#F7FAFC' w='100%'>
+            <Heading>Simple pricing for your business</Heading>
+            <Text>Plans that are carefully crafted to suit your business.</Text>
+        </Box>
+    )
+}
