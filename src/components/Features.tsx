59e9d7f385cb63b17bc1962cc1971025b73c94a4
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-27 17:59:39 +0100

    refactor: Use default theme unit for spacing

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -9,7 +9,7 @@ interface FeatureProps extends StackProps {
 export const Feature = (props: FeatureProps) => {
     const { children, icon, ...rest } = props;
     return (
-        <HStack {...rest} spacing={['1.98em', '1.98em', '1.5em']}>
+        <HStack {...rest} spacing={['8', '8', '6']}>
             <Icon as={icon} boxSize={'2.5rem'} />
             <Text fontSize='lg' fontWeight='bold' textAlign='left'>{children}</Text>
         </HStack>
@@ -19,8 +19,8 @@ export const Feature = (props: FeatureProps) => {
 
 export const Features = () => {
     return (
-        <Box maxW='994px' mx='auto' px='48px' my='3.5rem'>
-            <Stack color='#171923' spacing={['2.4em', '2.4em', '1.56rem']} direction={['column', 'column', 'row']}>
+        <Box maxW='994px' mx='auto' px='12' my='14'>
+            <Stack color='#171923' spacing={['10', '10', '6']} direction={['column', 'column', 'row']}>
                 <Feature icon={MoneyBackGuarantee}>
                     30 days money back Guarantee
                 </Feature>
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -4,8 +4,8 @@ import { CheckIcon } from "../icons/Icons";
 export const ListItem = (props: StackProps) => {
     const { children, ...rest } = props;
     return (
-        <HStack as='li' spacing='20px' align={'flex-start'} {...rest}>
-            <Icon as={CheckIcon} boxSize={'22px'} flexShrink={0} />
+        <HStack as='li' spacing='5' align={'flex-start'} {...rest}>
+            <Icon as={CheckIcon} boxSize={'1.375rem'} flexShrink={0} />
             <Text>{children}</Text>
         </HStack>
     )
@@ -13,20 +13,20 @@ export const ListItem = (props: StackProps) => {
 
 export const Pricing = () => {
     return (
-        <Box px='1.5rem'>
+        <Box px='6'>
             <Box maxW='994px' mx='auto' mt='-141px' borderRadius='xl' overflow='hidden' boxShadow='0px 10px 10px -5px rgba(0, 0, 0, 0.04), 0px 20px 25px -5px rgba(0, 0, 0, 0.10)'>
                 <Flex direction={['column', 'column', 'row']}>
-                    <Stack as='div' spacing='0.7rem' bg='#F0EAFB' px='3em' py={['2em', '2em', '3.5em']} w={[null, null, '378px']}>
-                        <Text fontSize='2xl' lineHeight='2rem' fontWeight='800' textAlign='center'>Premium PRO</Text>
-                        <Text fontSize={['5xl', '5xl', '6xl']} lineHeight='100%' fontWeight='800' textAlign='center'>$329</Text>
-                        <Text fontSize='lg' lineHeight='1.75rem' fontWeight='500' textAlign='center'>billed just once</Text>
-                        <Button bg='#805AD5' size='lg' color='#F7FAFC'  mt='0.8rem' _hover={{ bgColor: '#6053D0' }}>Get Started</Button>
+                    <Stack as='div' spacing='3' bg='#F0EAFB' px='3em' py={['2em', '2em', '3.5em']} w={[null, null, '378px']}>
+                        <Text fontSize='2xl' lineHeight='2rem' fontWeight='extrabold' textAlign='center'>Premium PRO</Text>
+                        <Text fontSize={['5xl', '5xl', '6xl']} lineHeight='100%' fontWeight='extrabold' textAlign='center'>$329</Text>
+                        <Text fontSize='lg' lineHeight='1.75rem' fontWeight='medium' textAlign='center'>billed just once</Text>
+                        <Button bg='#805AD5' size='lg' color='#F7FAFC'  mt='3' _hover={{ bgColor: '#6053D0' }}>Get Started</Button>
                     </Stack>
 
                     <Box color='#2D3748' bg='white' fontSize='lg' p='48px'>
-                        <Text mb='24px'>Access these features when you get this pricing package for your business.</Text>
+                        <Text mb='6'>Access these features when you get this pricing package for your business.</Text>
 
-                        <Stack as='ul' spacing='16px'>
+                        <Stack as='ul' spacing='4'>
                             <ListItem>International calling and messaging API</ListItem>
                             <ListItem>Additional phone numbers</ListItem>
                             <ListItem>Automated messages via Zapier</ListItem>
